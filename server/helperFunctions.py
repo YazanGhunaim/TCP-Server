30a0fb63c5d480f06922ffef3ceabbd0b21454ff
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-02-25 17:51:49 +0100

    catching exceptions while calc hashcode

--- server/helperFunctions.py
+++ server/helperFunctions.py
@@ -2,21 +2,26 @@ from constants import *
 
 
 def clientUserNameHashCode(username, key_id):
-    hashcode = 0
-    for i in range(len(username) - 2):
-        hashcode += ord(username[i])
+    try:
+        hashcode = 0
+        for i in range(len(username) - 2):
+            hashcode += ord(username[i])
 
-    hashcode *= 1000
-    hashcode %= 65536
-    expected = hashcode
+        hashcode *= 1000
+        hashcode %= 65536
+        expected = hashcode
 
-    server_key = SERVER_CLIENT_KEYS[key_id]['Server Key']
-    client_key = SERVER_CLIENT_KEYS[key_id]['Client Key']
+        server_key = SERVER_CLIENT_KEYS[key_id]['Server Key']
+        client_key = SERVER_CLIENT_KEYS[key_id]['Client Key']
 
-    hashcode = (hashcode + server_key) % 65536
-    expected = (expected + client_key) % 65536
+        hashcode = (hashcode + server_key) % 65536
+        expected = (expected + client_key) % 65536
 
-    return expected, hashcode
+        return expected, hashcode
+
+    except KeyError as e:
+        print(f"Error: {e}")
+        return None, None
 
 
 def hashCompare(hash1, hash2):
